3667e9e95b25a00a7f3cf43985bc75375829fd0b
Pandabug <71569878+Pandabug@users.noreply.github.com>
2021-05-02 20:55:45 +0200

    done

--- wine_app/lib/sreen/home_screen/useful_tools.dart
+++ wine_app/lib/sreen/home_screen/useful_tools.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../constants.dart';
-
 class UsefulTools extends StatelessWidget {
   const UsefulTools({
     Key key,
@@ -11,25 +9,27 @@ class UsefulTools extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: Row(
-        children: <Widget>[
-          UsefulToolsCard(
-            // image: 'assets/images/robot.jpg',
-            title: 'Sei indeciso?',
-          ),
-          UsefulToolsCard(
-            // image: 'winepp/assets/images/vino.jpg',
-            title: 'Fai un regalo',
-          ),
-          UsefulToolsCard(
-            // image: 'assets/winepp/assets/images/per_le_aziende.jpg',
-            title: 'Per le aziende',
-          ),
-          UsefulToolsCard(
-            // image: 'winepp/assets/images/consigli.jpg',
-            title: 'Consigli',
-          ),
-        ],
+      child: Container(
+        child: Row(
+          children: <Widget>[
+            UsefulToolsCard(
+              image: 'assets/images/robot.jpg',
+              title: 'Sei indeciso?',
+            ),
+            UsefulToolsCard(
+              image: 'assets/images/vino.jpg',
+              title: 'Fai un regalo',
+            ),
+            UsefulToolsCard(
+              image: 'assets/images/per_le_aziende.jpg',
+              title: 'Per le aziende',
+            ),
+            UsefulToolsCard(
+              image: 'assets/images/consigli.jpg',
+              title: 'Consigli',
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -46,46 +46,29 @@ class UsefulToolsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   margin: EdgeInsets.symmetric(
-    //     vertical: kDefaultPadding,
-    //     horizontal: kDefaultPadding * 0.5,
-    //   ),
-    //   height: 80,
-    //   width: 80,
-    //   child: ClipRRect(
-    //     borderRadius: BorderRadius.circular(50),
-    //     child: Image.asset(
-    //       image,
-    //       fit: BoxFit.cover,
-    //     ),
-    //   ),
-    // );
     return Container(
-      margin: EdgeInsets.symmetric(
-        vertical: kDefaultPadding,
-        horizontal: kDefaultPadding * 0.2,
-      ),
-      height: 120,
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      height: 130,
       width: 110,
       child: Column(
         children: <Widget>[
           Container(
-            margin: EdgeInsets.only(bottom: kDefaultPadding * 0.3),
             width: 90,
             height: 90,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(50),
-              // image: DecorationImage(
-              //   fit: BoxFit.cover,
-              //   image: AssetImage(image),
-              // ),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(image),
+              ),
             ),
           ),
+          SizedBox(height: 10),
           Text(
             title,
             style: TextStyle(
-              fontSize: 13,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ],
