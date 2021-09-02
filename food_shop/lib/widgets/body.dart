ea360eb3296921d542189d02e8beba70c557c9f8
Bednarchuk Ivan-Vitaliy <xyjiigah.ib@gmail.com>
2021-09-03 00:37:34 +0200

    done

--- food_shop/lib/widgets/body.dart
+++ food_shop/lib/widgets/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_shop/widgets/discover_food.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -9,236 +10,177 @@ class Body extends StatelessWidget {
         top: 50,
         right: 25,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Icon(
-                Icons.dashboard_customize_outlined,
-                size: 30,
-              ),
-              Icon(
-                Icons.shopping_bag_outlined,
-                size: 30,
-              ),
-            ],
-          ),
-          SizedBox(height: 30),
-          Text(
-            'Ice cream Lover?',
-            style: TextStyle(
-              fontSize: 35,
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(
+                  Icons.dashboard_customize_outlined,
+                  size: 30,
+                ),
+                Icon(
+                  Icons.shopping_bag_outlined,
+                  size: 30,
+                ),
+              ],
             ),
-          ),
-          Text(
-            'Order & Eat.',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w400,
-              color: Colors.black,
+            SizedBox(height: 30),
+            Text(
+              'Ice cream Lover?',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
             ),
-          ),
-          SizedBox(height: 15),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  height: 55,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextField(
-                    onChanged: (value) => {},
-                    decoration: InputDecoration(
-                      hintText: 'Search your food',
-                      fillColor: Colors.red,
-                      // filled: true,
-                      contentPadding: EdgeInsets.only(
-                        top: 15,
-                        left: 30,
+            Text(
+              'Order & Eat.',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 15),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    height: 55,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: TextField(
+                      onChanged: (value) => {},
+                      decoration: InputDecoration(
+                        hintText: 'Search your food',
+                        fillColor: Colors.red,
+                        // filled: true,
+                        contentPadding: EdgeInsets.only(
+                          top: 15,
+                          left: 30,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                          size: 32,
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
                       ),
-                      suffixIcon: Icon(
-                        Icons.search,
-                        color: Colors.black,
-                        size: 32,
-                      ),
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
                     ),
                   ),
                 ),
-              ),
-              SizedBox(width: 10),
-              Container(
-                height: 55,
-                width: 55,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.white,
+                SizedBox(width: 10),
+                Container(
+                  height: 55,
+                  width: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    Icons.align_vertical_bottom_rounded,
+                  ),
                 ),
-                child: Icon(
-                  Icons.align_vertical_bottom_rounded,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 40),
+              ],
+            ),
+            SizedBox(height: 40),
+            DiscoverFood(),
+            SizedBox(height: 40),
+            WeRecommend(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WeRecommend extends StatelessWidget {
+  const WeRecommend({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
           Text(
-            'Discover food',
+            'We Recommend',
             style: TextStyle(
               fontSize: 25,
               fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 20),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
               children: <Widget>[
-                FoodDiscover(title: 'Vanilla flaviour'),
-                FoodDiscover(title: 'Chocolate flavior'),
-                FoodDiscover(title: 'Chocolate milk'),
+                RecomendedFood(),
               ],
             ),
           ),
-          SizedBox(height: 20),
-          Wrap(
-            children: <Widget>[
-              FoodList(
-                title: 'Scoops',
-                price: '5',
-              ),
-              FoodList(
-                title: 'Scoops',
-                price: '10',
-              ),
-            ],
-          ),
+          SizedBox(height: 40),
         ],
       ),
     );
   }
 }
 
-class FoodList extends StatelessWidget {
-  const FoodList({
+class RecomendedFood extends StatelessWidget {
+  const RecomendedFood({
     Key? key,
-    required this.title,
-    required this.price,
   }) : super(key: key);
-
-  final String title, price;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         Container(
-          height: 230,
-          width: 150,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.white,
-          ),
-          child: Column(
+          width: 220,
+          height: 100,
+          color: Colors.red,
+          child: Row(
             children: <Widget>[
-              SizedBox(height: 20),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/Popsicles.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/Scoops.jpg'),
               ),
             ],
           ),
         ),
         Positioned(
-          bottom: 15,
-          left: 30,
+          right: 30,
+          top: 20,
           child: Column(
             children: <Widget>[
               Text(
-                title,
+                'Japanese',
                 style: TextStyle(
-                  fontSize: 17,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black,
                 ),
               ),
               Text(
                 'Starting From',
                 style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.grey,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
                 ),
               ),
-              SizedBox(height: 5),
-              Text(
-                '\$$price',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              )
             ],
           ),
         ),
       ],
-    );
-  }
-}
-
-class FoodDiscover extends StatelessWidget {
-  const FoodDiscover({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Colors.white,
-      ),
-      margin: EdgeInsets.symmetric(horizontal: 3),
-      child: Padding(
-        padding: EdgeInsets.only(
-          left: 20,
-          right: 10,
-        ),
-        child: Row(
-          children: <Widget>[
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 13,
-              ),
-            ),
-            SizedBox(width: 10),
-            Icon(
-              Icons.close,
-              size: 17,
-              color: Colors.black,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ food_shop/lib/widgets/discover_food.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class DiscoverFood extends StatelessWidget {
+  const DiscoverFood({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Discover food',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: <Widget>[
+              FoodDiscover(title: 'Vanilla flaviour'),
+              FoodDiscover(title: 'Chocolate flavior'),
+              FoodDiscover(title: 'Chocolate milk'),
+            ],
+          ),
+        ),
+        SizedBox(height: 20),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: <Widget>[
+              FoodList(
+                  image: AssetImage('assets/Scoops.jpg'),
+                  title: 'Scoops',
+                  price: '5'),
+              FoodList(
+                  image: AssetImage('assets/Popsicles.jpg'),
+                  title: 'Popsicles',
+                  price: '10'),
+              FoodList(
+                  image: AssetImage('assets/Scoops.jpg'),
+                  title: 'Popsicles',
+                  price: '7'),
+              FoodList(
+                  image: AssetImage('assets/Popsicles.jpg'),
+                  title: 'Scoops',
+                  price: '12'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FoodList extends StatelessWidget {
+  const FoodList({
+    Key? key,
+    required this.title,
+    required this.price,
+    required this.image,
+  }) : super(key: key);
+
+  final String title, price;
+  final AssetImage image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 230,
+          width: 160,
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 40),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 10,
+          right: 10,
+          child: Icon(
+            Icons.favorite_border_sharp,
+          ),
+        ),
+        Positioned(
+          bottom: 15,
+          left: 30,
+          child: Column(
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                'Starting From',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                '\$$price',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FoodDiscover extends StatelessWidget {
+  const FoodDiscover({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+      ),
+      margin: EdgeInsets.symmetric(horizontal: 3),
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 20,
+          right: 10,
+        ),
+        child: Row(
+          children: <Widget>[
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 13,
+              ),
+            ),
+            SizedBox(width: 10),
+            Icon(
+              Icons.close,
+              size: 17,
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
